8fe228d536d491166bc86f2809b87159e2be7946
harshadBOL <harshad.pawar@lji.io>
2023-02-13 12:17:46 +0530

    profile image fun added

--- UserProfile/app/src/main/java/com/example/userprofile/MainActivity.kt
+++ UserProfile/app/src/main/java/com/example/userprofile/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.userprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.userprofile.ui.theme.UserProfileTheme
@@ -42,8 +47,33 @@ fun ProfileCard() {
             backgroundColor = Color.White,
             elevation = 4.dp
         ) {
-//
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ProfileImageFunction()
+            }
         }
+    }
+}
+
+@Composable
+private fun ProfileImageFunction() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = Color.White
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_images),
+            contentDescription = "user Profile image",
+            modifier = Modifier.size(135.dp)
+        )
     }
 }
 
